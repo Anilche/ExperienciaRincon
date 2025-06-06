2f1c1e26e1c2d1a3026ea32f4a02508454447d1c
Anilche <giammariniana@gmail.com>
2025-06-06 17:25:06 -0300

    Aparece y desaparece botón de interaccion x proximidad
    quiero subir esto antes de reemplazar el gitignore por si se rompe todo

---
+++ Experiencia-Rincon/Assets/Scripts/DialogoTrigger.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogoTrigger : MonoBehaviour
+{
+
+    [Header("Boton Interaccion")]
+    [SerializeField] private GameObject botonInteraccion;
+
+    [Header("INK JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+    private bool jugadorEnRango;
+
+    // Al iniciar, se desactiva el botón de interaccion y se setea que el jugador no está en rano de interacción
+    private void Awake()
+    {
+        jugadorEnRango = false;
+        botonInteraccion.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // Si el jugador está en rango, se mostrará el botón de interacción, sino, no se mostrará
+        if (jugadorEnRango)
+        {
+            botonInteraccion.SetActive(true);
+
+            // Si el jugador presiona la tecla E, se activa el diálogo
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                // ActivarDialogo();
+                Debug.Log("Activar diálogo con el JSON: " + inkJSON.text);
+            }
+        }
+        // Si el jugador no está en rango, se desactiva el botón de interacción
+        else
+        {
+            botonInteraccion.SetActive(false);
+        }
+
+    }
+
+    // Al entrar en el trigger, se activa el botón de interacción y se setea que el jugador está en rango
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            jugadorEnRango = true;
+        }
+    }
+
+    // Al salir del trigger se setea que el jugador no está en rango
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            jugadorEnRango = false;
+        }
+    }
+
+}
