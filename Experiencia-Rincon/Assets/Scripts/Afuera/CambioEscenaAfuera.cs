4cc9ddbc3438c1aa41789bf0ff3fbd79df7d1704
Anilche <giammariniana@gmail.com>
2025-10-17 23:34:49 -0300

    Cambio de escena

---
+++ Experiencia-Rincon/Assets/Scripts/Afuera/CambioEscenaAfuera.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioEscenaAfuera : MonoBehaviour
+{
+
+    [SerializeField] private CanvasGroup canvasGroupIn;
+
+    [SerializeField] private CanvasGroup canvasGroupOut;
+
+    [SerializeField] private float fadeDuration = 5.0f;
+
+    private void Start()
+    {
+        FadeIn();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FadeOut();
+        }
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeCanvasGroup(canvasGroupIn, canvasGroupIn.alpha, 0, fadeDuration));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeCanvasGroup(canvasGroupOut, canvasGroupOut.alpha, 0, fadeDuration));
+        SceneManager.LoadScene("Experiencia");
+    }
+
+    private IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end, float duration)
+    {
+        float elapsedTime = 0.0f;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            cg.alpha = Mathf.Lerp(start, end, elapsedTime / duration);
+            yield return null;
+        }
+        cg.alpha = end;
+    }
+
+    
+}
