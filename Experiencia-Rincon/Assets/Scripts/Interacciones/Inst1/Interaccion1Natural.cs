29026217584a663c5c0e75bdded2f058191e1e91
Anilche <giammariniana@gmail.com>
2025-11-03 22:18:35 -0300

    Ramitas y manzanas recogibles!!

---
+++ Experiencia-Rincon/Assets/Scripts/Interacciones/Inst1/Interaccion1Natural.cs
@@ -0,0 +1,230 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Interaccion1Natural : MonoBehaviour
+{
+    private Transform highlight;
+    private Transform selection;
+    private RaycastHit raycastHit;
+
+    //private bool estaEnAreaDeInteraccion = false;
+    private int contadorManzanasRecogidas = 0;
+
+    [Header("Manzanas")]
+    [SerializeField] public GameObject manzana1;
+    [SerializeField] public GameObject manzana2;
+    [SerializeField] public GameObject manzana3;
+    [SerializeField] public GameObject manzana4;
+    [SerializeField] public GameObject manzana5;
+    [SerializeField] public GameObject manzana6;
+
+    [Header("Montones Manzanas Canasta")]
+    [SerializeField] public GameObject montonManzanas1;
+    [SerializeField] public GameObject montonManzanas2;
+    [SerializeField] public GameObject montonManzanas3;
+
+    [Header("Distancia máxima de interacción")]
+    [SerializeField] private float distanciaMaxima = 3f; // límite de alcance
+
+    [Header("Requerimientos para utilizarse")]
+    [SerializeField] public int numFaseNecesaria; // Requerimiento para poder activar el trigger de elecciones
+
+    private Camera camara;
+
+    AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        camara = Camera.main;
+    }
+
+    void Update()
+    {
+        if (GameManager.GetInstance().faseAhora >= numFaseNecesaria)
+        {
+            manzana1.GetComponent<Rigidbody>().isKinematic = false;
+            manzana2.GetComponent<Rigidbody>().isKinematic = false;
+            manzana3.GetComponent<Rigidbody>().isKinematic = false;
+            manzana4.GetComponent<Rigidbody>().isKinematic = false;
+            manzana5.GetComponent<Rigidbody>().isKinematic = false;
+            manzana6.GetComponent<Rigidbody>().isKinematic = false;
+        }
+            
+        if (GameManager.GetInstance().faseAhora >= numFaseNecesaria /*&& estaEnAreaDeInteraccion*/)
+        {
+            // Highlight
+            if (highlight != null)
+            {
+                highlight.gameObject.GetComponent<Outline>().enabled = false;
+                highlight = null;
+            }
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)) //Make sure you have EventSystem in the hierarchy before using EventSystem
+            {
+                highlight = raycastHit.transform;
+
+                float distancia = Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, highlight.position);
+
+                if (highlight.CompareTag("Seleccionable") && distancia <= distanciaMaxima)
+                {
+                    if (highlight.gameObject.GetComponent<Outline>() != null)
+                    {
+                        highlight.gameObject.GetComponent<Outline>().enabled = true;
+                    }
+                    else
+                    {
+                        Outline outline = highlight.gameObject.AddComponent<Outline>();
+                        outline.enabled = true;
+                        highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.yellow;
+                        highlight.gameObject.GetComponent<Outline>().OutlineWidth = 7.0f;
+                    }
+                }
+                else
+                {
+                    highlight = null;
+                }
+            }
+
+            // Selection
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+                float distancia = Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, highlight.position);
+
+                if (highlight && distancia <= distanciaMaxima)
+                {
+                    if (selection != null)
+                    {
+                        selection.gameObject.GetComponent<Outline>().enabled = false;
+                    }
+                    selection = raycastHit.transform;
+                    selection.gameObject.GetComponent<Outline>().enabled = true;
+
+                    Debug.Log(highlight.gameObject);
+
+                    string objetoSeleccionado = highlight.gameObject.name;
+
+                    switch (objetoSeleccionado)
+                    {
+                        case "Manzana1":
+                            selection.gameObject.GetComponent<Outline>().enabled = false;
+
+                            audioManager.PlaySFX(audioManager.seleccionSFX);
+
+                            manzana1.SetActive(false);
+                            contadorManzanasRecogidas++;
+                            Debug.Log("Manzanas recogidas: " + contadorManzanasRecogidas);
+                            break;
+
+                        case "Manzana2":
+                            selection.gameObject.GetComponent<Outline>().enabled = false;
+
+                            audioManager.PlaySFX(audioManager.seleccionSFX);
+
+                            manzana2.SetActive(false);
+                            contadorManzanasRecogidas++;
+                            Debug.Log("Manzanas recogidas: " + contadorManzanasRecogidas);
+                            break;
+
+                        case "Manzana3":
+                            selection.gameObject.GetComponent<Outline>().enabled = false;
+
+                            audioManager.PlaySFX(audioManager.seleccionSFX);
+
+                            manzana3.SetActive(false);
+                            contadorManzanasRecogidas++;
+                            Debug.Log("Manzanas recogidas: " + contadorManzanasRecogidas);
+                            break;
+
+                        case "Manzana4":
+                            selection.gameObject.GetComponent<Outline>().enabled = false;
+
+                            audioManager.PlaySFX(audioManager.seleccionSFX);
+
+                            manzana4.SetActive(false);
+                            contadorManzanasRecogidas++;
+                            Debug.Log("Manzanas recogidas: " + contadorManzanasRecogidas);
+                            break;
+
+                        case "Manzana5":
+                            selection.gameObject.GetComponent<Outline>().enabled = false;
+
+                            audioManager.PlaySFX(audioManager.seleccionSFX);
+
+                            manzana5.SetActive(false);
+                            contadorManzanasRecogidas++;
+                            Debug.Log("Manzanas recogidas: " + contadorManzanasRecogidas);
+                            break;
+
+                        case "Manzana6":
+                            selection.gameObject.GetComponent<Outline>().enabled = false;
+
+                            audioManager.PlaySFX(audioManager.seleccionSFX);
+
+                            manzana6.SetActive(false);
+                            contadorManzanasRecogidas++;
+                            Debug.Log("Manzanas recogidas: " + contadorManzanasRecogidas);
+                            break;
+
+                        default:
+                            Debug.Log("Objeto no reconocido");
+                            break;
+                    }
+
+                    highlight = null;
+                }
+                else
+                {
+                    if (selection)
+                    {
+                        selection.gameObject.GetComponent<Outline>().enabled = false;
+                        selection = null;
+                    }
+                }
+            }
+        }
+
+        if (contadorManzanasRecogidas == 1)
+        {
+            montonManzanas1.SetActive(true);
+        }
+        if (contadorManzanasRecogidas == 3)
+        {
+            montonManzanas2.SetActive(true);
+        }
+        if (contadorManzanasRecogidas == 6)
+        {
+            montonManzanas3.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && GameManager.GetInstance().faseAhora >= numFaseNecesaria)
+        {
+            //estaEnAreaDeInteraccion = true;
+            Debug.Log("Jugador en area de interaccion natural");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && GameManager.GetInstance().faseAhora >= numFaseNecesaria)
+        {
+            //estaEnAreaDeInteraccion = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //estaEnAreaDeInteraccion = false;
+            Debug.Log("Jugador salio del area de interaccion natural");
+        }
+    }
+}
