89273e65e2eb46df91c05b313e6377475b7d858f
Anilche <giammariniana@gmail.com>
2025-11-05 19:18:05 -0300

    Colliders arreglados + Update

---
+++ Experiencia-Rincon/Assets/Scripts/Interacciones/Inst1/Interaccion1Neutral.cs
@@ -0,0 +1,153 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Interaccion1Neutral : MonoBehaviour
+{
+    private Transform highlight;
+    private Transform selection;
+    private RaycastHit raycastHit;
+
+    private bool estaEnAreaDeInteraccion = false;
+
+    private string tagSeleccionable = "Seleccionable";
+
+    [Header("Moneda")]
+    [SerializeField] public GameObject moneda;
+
+    [Header("Animator Moneda")]
+    [SerializeField] public Animator animMoneda;
+
+    [Header("Distancia máxima de interacción")]
+    [SerializeField] private float distanciaMaxima = 3f; // límite de alcance
+
+    [Header("Requerimientos para utilizarse")]
+    [SerializeField] public int numFaseNecesaria; // Requerimiento para poder activar el trigger de elecciones
+
+    private Camera camara;
+
+    AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        camara = Camera.main;
+    }
+
+    void Update()
+    {
+
+        if (GameManager.GetInstance().faseAhora >= numFaseNecesaria && estaEnAreaDeInteraccion)
+        {
+            moneda.tag = tagSeleccionable;
+
+            // Highlight
+            if (highlight != null)
+            {
+                highlight.gameObject.GetComponent<Outline>().enabled = false;
+                highlight = null;
+            }
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)) //Make sure you have EventSystem in the hierarchy before using EventSystem
+            {
+                highlight = raycastHit.transform;
+
+                float distancia = Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, highlight.position);
+
+                if (highlight.CompareTag("Seleccionable") && distancia <= distanciaMaxima)
+                {
+                    if (highlight.gameObject.GetComponent<Outline>() != null)
+                    {
+                        highlight.gameObject.GetComponent<Outline>().enabled = true;
+                    }
+                    else
+                    {
+                        Outline outline = highlight.gameObject.AddComponent<Outline>();
+                        outline.enabled = true;
+                        highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.yellow;
+                        highlight.gameObject.GetComponent<Outline>().OutlineWidth = 7.0f;
+                    }
+                }
+                else
+                {
+                    highlight = null;
+                }
+            }
+
+            // Selection
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+                float distancia = Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, highlight.position);
+
+                if (highlight && distancia <= distanciaMaxima)
+                {
+                    if (selection != null)
+                    {
+                        selection.gameObject.GetComponent<Outline>().enabled = false;
+                    }
+                    selection = raycastHit.transform;
+                    selection.gameObject.GetComponent<Outline>().enabled = true;
+
+                    Debug.Log(highlight.gameObject);
+
+                    string objetoSeleccionado = highlight.gameObject.name;
+
+                    switch (objetoSeleccionado)
+                    {
+                        case "monedaFuente":
+                            selection.gameObject.GetComponent<Outline>().enabled = false;
+
+                            audioManager.PlaySFX(audioManager.seleccionSFX);
+                            animMoneda.SetBool("TirarMoneda", true);
+
+                            break;
+
+                        default:
+                            Debug.Log("Objeto no reconocido");
+                            break;
+                    }
+
+                    highlight = null;
+                }
+                else
+                {
+                    if (selection)
+                    {
+                        selection.gameObject.GetComponent<Outline>().enabled = false;
+                        selection = null;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && GameManager.GetInstance().faseAhora >= numFaseNecesaria)
+        {
+            estaEnAreaDeInteraccion = true;
+            Debug.Log("Jugador en area de interaccion natural");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && GameManager.GetInstance().faseAhora >= numFaseNecesaria)
+        {
+            estaEnAreaDeInteraccion = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            estaEnAreaDeInteraccion = false;
+            Debug.Log("Jugador salio del area de interaccion natural");
+        }
+    }
+}
