f2903f44c2146680fc735eba1b3ab089c00ab52e
Anilche <giammariniana@gmail.com>
2025-11-16 17:59:34 -0300

    Recien empece la instancia descanso

---
+++ Experiencia-Rincon/Assets/Scripts/Descanso/InstDescanso.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InstDescanso : MonoBehaviour
+{
+    private Transform highlight;
+    private Transform selection;
+    private RaycastHit raycastHit;
+
+    // private string tagSeleccionable = "Seleccionable";
+
+    [Header("Animator Instrumento")]
+    [SerializeField] public Animator animInstrumento;
+
+    [Header("Distancia máxima de interacción")]
+    [SerializeField] private float distanciaMaxima = 3f; // límite de alcance
+
+    [Header("Requerimientos para utilizarse")]
+    [SerializeField] public int numFaseNecesaria; // Requerimiento para poder activar el trigger de elecciones
+
+    private Camera camara;
+
+    AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        camara = Camera.main;
+    }
+
+    void Update()
+    {
+
+        if (GameManager.GetInstance().faseAhora >= numFaseNecesaria /*&& estaEnAreaDeInteraccion*/)
+        {
+
+            // Highlight
+            if (highlight != null)
+            {
+                highlight.gameObject.GetComponent<Outline>().enabled = false;
+                highlight = null;
+            }
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)) //Make sure you have EventSystem in the hierarchy before using EventSystem
+            {
+                highlight = raycastHit.transform;
+
+                float distancia = Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, highlight.position);
+
+                if (highlight.CompareTag("Seleccionable") && distancia <= distanciaMaxima)
+                {
+                    if (highlight.gameObject.GetComponent<Outline>() != null)
+                    {
+                        highlight.gameObject.GetComponent<Outline>().enabled = true;
+                    }
+                    else
+                    {
+                        Outline outline = highlight.gameObject.AddComponent<Outline>();
+                        outline.enabled = true;
+                        highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.yellow;
+                        highlight.gameObject.GetComponent<Outline>().OutlineWidth = 7.0f;
+                    }
+                }
+                else
+                {
+                    highlight = null;
+                }
+            }
+
+            // Selection
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+                float distancia = Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, highlight.position);
+
+                if (highlight && distancia <= distanciaMaxima)
+                {
+                    if (selection != null)
+                    {
+                        selection.gameObject.GetComponent<Outline>().enabled = false;
+                    }
+                    selection = raycastHit.transform;
+                    selection.gameObject.GetComponent<Outline>().enabled = true;
+
+                    //Debug.Log(highlight.gameObject);
+
+                    string objetoSeleccionado = highlight.gameObject.name;
+
+                    switch (objetoSeleccionado)
+                    {
+                        case "Mixer":
+                            selection.gameObject.GetComponent<Outline>().enabled = false;
+
+                            StartCoroutine(hacerAnimacion());
+
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    highlight = null;
+                }
+                else
+                {
+                    if (selection)
+                    {
+                        selection.gameObject.GetComponent<Outline>().enabled = false;
+                        selection = null;
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator hacerAnimacion()
+    {
+        animInstrumento.SetBool("tocar", true);
+        yield return new WaitForSeconds(2f);
+        animInstrumento.SetBool("tocar", false);
+    }
+}
